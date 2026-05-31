e2d7b86f004e3e6a604941d181bbdc3ea4f70e14
SantiagouJ <santiagojuradorodriguez3@gmail.com>
2026-05-30 21:57:52 -0500

    fix

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -2,4 +2,4 @@ import postsData from './posts.json'
 import { resolveFeedPosts } from './resolveMedia'
 import type { FeedPost } from '../types/feed'
 
-export const FEED_POSTS = resolveFeedPosts(postsData as FeedPost[])
+export const FEED_POSTS = resolveFeedPosts(postsData as unknown as FeedPost[])
